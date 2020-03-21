c4411bb3c8fe75010afad94da05b0c6769630072
wimalopaan <wilhelm.wm.meier@googlemail.com>
2020-03-21 08:03:57 +0100

    GPL inserted

--- SCRIPTS/TELEMETRY/ps_tel.lua
+++ SCRIPTS/TELEMETRY/ps_tel.lua
@@ -1,4 +1,23 @@
-gPulseSw = {0, 0, 0, 0};
+--
+-- WM OTXE - OpenTX Extensions 
+-- Copyright (C) 2020 Wilhelm Meier <wilhelm.wm.meier@googlemail.com>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+ 
+
+ gPulseSw = {0, 0, 0, 0};
 
 --- define menu
 
@@ -93,6 +112,7 @@ local function init()
 
    for i,p in ipairs(menu.pages) do
       for k, item in ipairs(p) do
+	 item.cb = select;
 	 sstate.switches[#sstate.switches + 1] = item;
       end
    end
