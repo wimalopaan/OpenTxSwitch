ce00b17064628f401a3f6b85fe7fd3070946cbfd
wimalopaan <wilhelm.wm.meier@googlemail.com>
2020-08-28 11:36:16 +0200

    resurrected tiptip mode (not yet tested)

---
+++ MODELS/tpstds.lua
@@ -0,0 +1,44 @@
+local name = "Def";
+
+local gVariable = 5;
+
+local useSbus = 0; 
+
+local gstates1 = {"aus", "ein", "blink1", "blink2"};
+local gstates2 = {"aus", "ein 1", "ein 2"};
+
+local menu = {
+   title = "WM MultiTip",
+   
+   scrollUpDn = "ls", -- speedDials: direct navigating
+   scrollLR = "rs",
+   
+   pageSwitch = "6p",
+   
+   state = {
+      activeRow = 1,
+      activeCol = 1,
+      activePage = nil
+   },
+   
+   pages = {
+      {
+	 items = { 
+	    {name = "Fun A", states = gstates2, state = 1, data = {switch = "sa", count = 1, offState = 1, module = 1}},
+	    {name = "Fun B", states = gstates2, state = 1, data = {switch = "sb", count = 2, offState = 1, module = 1}},
+	    {name = "Fun C", states = gstates2, state = 1, data = {switch = nil, count = 3, offState = 1, module = 1}},
+	    {name = "Fun D", states = gstates2, state = 1, data = {switch = nil, count = 4, offState = 1, module = 1}},
+	    {name = "Fun E", states = gstates2, state = 1, data = {switch = nil, count = 5, offState = 1, module = 1}},
+	    {name = "Fun F", states = gstates2, state = 1, data = {switch = nil, count = 6, offState = 1, module = 1}},
+	 }
+      },
+      {
+	 items = { 
+	    {name = "Fun G", states = gstates2, state = 1, data = {switch = nil, count = 7, offState = 1, module = 1}},
+	    {name = "Fun H", states = gstates2, state = 1, data = {switch = nil, count = 8, offState = 1, module = 1}}
+	 }
+      }
+   }
+}   
+
+return {name = name, menu = menu, gVar = gVariable, useSbus = useSbus};
---
+++ SCRIPTS/TELEMETRY/wmtip.lua
@@ -0,0 +1,40 @@
+--
+-- WM OTXE - OpenTX Extensions 
+-- Copyright (C) 2020 Wilhelm Meier <wilhelm.wm.meier@googlemail.com>
+--
+
+-- This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. 
+-- To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ 
+-- or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+-- IMPORTANT
+-- Please note that the above license also covers the transfer protocol used and the encoding scheme and 
+-- all further principals of tranferring state and other information.
+
+
+local pie = {};
+pie.zone = {};
+pie.zone.x = 0;
+pie.zone.y = 0;
+pie.zone.w = LCD_W;
+pie.zone.h = LCD_H;
+pie.zone.fh = 8;
+pie.zone.y_offset = 8;
+pie.zone.y_poffset = 0;
+
+local widget = nil;
+
+local function run_telemetry(event)
+   widget.procAndDisplay(event, pie);
+end
+
+local function init_telemetry()
+  widget = loadfile("/WIDGETS/WMSWT/main.lua")();
+  widget.init(nil);
+end
+
+local function background_telemetry()
+  widget.background();
+end
+
+return {run=run_telemetry, init=init_telemetry, background=background_telemetry}
